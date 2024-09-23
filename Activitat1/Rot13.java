4156419d77debc312aef5774ca6b176fbfd7834b
Ignacio Loayza <loayzajoseignacio3@gmail.com>
2024-09-23 19:13:45 +0200

    Rot13 parcialmente refaccionado

--- Activitat1/Rot13.java
+++ Activitat1/Rot13.java
@@ -1,52 +1,93 @@
 package Activitat1;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Rot13{
-    private final String alfabeto = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,u,v,w,x,y,z"; 
+    private final String alfabeto = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,u,v,w,x,y,z,á,à,é,è,í,ì,ï,ó,ò,ú,ù,ü"; 
     private final String[] alfabetoMin = alfabeto.split(",");
     private final String[] alfabetoMax = alfabeto.toUpperCase().split(",");
+    private static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception{
+        System.out.println("Un texto");
+        String mensaje = entrada.readLine();
+        System.out.println(mensaje);
+        Rot13 xifra = new Rot13();
+        mensaje = xifra.xifraRot13(mensaje);
+        System.out.println(mensaje);
+        mensaje = xifra.descifraRot13(mensaje);
+        System.out.println(mensaje);
+    }
+
     public String xifraRot13(String mensaje){
-        String nuevoMensaje = "";
+        StringBuilder nuevoMensaje = new StringBuilder(mensaje.length());
         for(int i = 0; i < mensaje.length(); i++){
             char letra = mensaje.charAt(i);
             if(Character.isLetter(letra)){
-                boolean hoEs = Character.isUpperCase(i);
-                if(hoEs){
-                    for(int j = 0 ; j < alfabetoMax.length; j++){
-                        if(alfabetoMax[j].equals(Character.toString(letra))){
-                           nuevoMensaje = nuevoMensaje + alfabetoMax[(j + 13)%alfabetoMax.length]; 
-                        }
-                    }
-                }else{
-                    for(int j = 0 ; j < alfabetoMin.length; j++){
-                        if(alfabetoMin[j].equals(Character.toString(letra))){
-                           nuevoMensaje = nuevoMensaje + alfabetoMin[(j + 13)%alfabetoMin.length]; 
-                        }
-                    }
-                }
+                nuevoMensaje.append(cifraLetra(letra));
             } else{
-                nuevoMensaje = nuevoMensaje + letra; 
+                nuevoMensaje.append(letra);
             }
+        }
+        return nuevoMensaje.toString();
+    }
+
+    public String descifraRot13(String mensaje){
+        StringBuilder nuevoMensaje = new StringBuilder(mensaje.length()); 
+        for(int i = 0; i < mensaje.length(); i++){
+            char letra = mensaje.charAt(i);
+            if(Character.isLetter(letra)){
+                nuevoMensaje.append(descifraLetra(letra));
+            } else{
+                nuevoMensaje.append(letra); 
+            }
+        }
+        return nuevoMensaje.toString();
+    }
+
+    private String cifraLetra(char letra){
+        String nuevoMensaje = "";
+        if( Character.isUpperCase(letra)){
+            for(int j = 0 ; j < alfabetoMax.length; j++){
+                if(alfabetoMax[j].equals(Character.toString(letra))){
+                   nuevoMensaje = alfabetoMax[(j + 13)%alfabetoMax.length]; 
+                }
+            }
+        }else if(Character.isLowerCase(letra)){
+            for(int j = 0 ; j < alfabetoMin.length; j++){
+                if(alfabetoMin[j].equals(Character.toString(letra))){
+                   nuevoMensaje = alfabetoMin[(j + 13)%alfabetoMin.length]; 
+                }
+            }
+        }else{
+
         }
         return nuevoMensaje;
     }
-    public String descifraRot13(String mensaje){
+
+    private String descifraLetra(char letra){
         String nuevoMensaje = "";
-        for(int i = 0; i < mensaje.length(); i++){
-            char letra = mensaje.charAt(i);
-            if(Character.isLetter(i)){
-                boolean hoEs = Character.isUpperCase(i);
-                if(hoEs){
-                    for(int j = 0 ; j < alfabetoMax.length; j++){
-                        if(alfabetoMax[j].equals(Character.toString(letra))){
-                           nuevoMensaje = nuevoMensaje + alfabetoMax[(j - 13)%alfabetoMax.length]; 
-                        }
+        if(Character.isUpperCase(letra)){
+            for(int j = 0 ; j < alfabetoMax.length; j++){
+                if(alfabetoMax[j].equals(Character.toString(letra))){
+                    int result = j -13;
+                    if(result < 0){
+                        nuevoMensaje = alfabetoMax[alfabetoMax.length - result];
+                    } else{
+                        nuevoMensaje = alfabetoMax[result];
                     }
-                }else{
-                    for(int j = 0 ; j < alfabetoMin.length; j++){
-                        if(alfabetoMin[j].equals(Character.toString(letra))){
-                           nuevoMensaje = nuevoMensaje + alfabetoMin[(j - 13)%alfabetoMin.length]; 
-                        }
-                    }
+                }
+            }
+        }else{
+            for(int j = 0 ; j < alfabetoMin.length; j++){
+                if(alfabetoMin[j].equals(Character.toString(letra))){
+                    int result = j -13;
+                    if(result < 0){
+                        nuevoMensaje = alfabetoMin[alfabetoMin.length - result];
+                    } else{
+                        nuevoMensaje = alfabetoMin[result];
+                    } 
                 }
             }
         }
